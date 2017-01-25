7f6efe5ca9d1e877eb20a51fb80b5e0a8930a443
yeffasol <yeffasol@mail.ru>
2017-01-25 20:51:38 +0300

    Add header, promo, products

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -2,6 +2,8 @@ export default function menu() {
 
   const doc = document;
   let menu = doc.querySelector('.menu');
+  let headerSearch = doc.querySelector('.header__search');
+  let headerBasket= doc.querySelector('.header__basket');
   doc.querySelector('.menu__toggle').addEventListener('click', _doSome);
 
   menu.classList.remove('menu--no-js');
@@ -9,9 +11,13 @@ export default function menu() {
     if (menu.classList.contains('menu--closed')) {
       menu.classList.remove("menu--closed");
       menu.classList.add("menu--opened");
+      headerBasket.classList.toggle('display-none');
+      headerSearch.classList.toggle('display-none');
     } else {
       menu.classList.add("menu--closed");
       menu.classList.remove("menu--opened");
+      headerBasket.classList.toggle('display-none');
+      headerSearch.classList.toggle('display-none');
     }
   }
 }
